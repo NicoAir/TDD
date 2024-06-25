baa1325c9042d1a578617993a8eb3fcbda722f0d
Nico <n-augier@hotmail.fr>
2024-06-25 13:34:28 +0200

    impl

--- kata2.py
+++ kata2.py
@@ -4,5 +4,15 @@ def add(numbers: str) -> int:
     if not numbers:
         return 0
     else:
-        numbers_list = re.split(',|\n', numbers)
-        return sum(int(num) for num in numbers_list)
+        delimiter = ',|\n'
+        if numbers.startswith('//'):
+            delimiter, numbers = numbers.split('\n', 1)
+            delimiter = delimiter[2:]
+            numbers_list = re.split(re.escape(delimiter), numbers)
+        else:
+            numbers_list = re.split(delimiter, numbers)
+        for num in numbers_list:
+            if not num.isdigit():
+                    position = numbers.find(num)
+                    raise ValueError(f"'{delimiter}' expected but '{num}' found at position {position}.")
+        return sum(int(num) for num in numbers_list if num.isdigit())
