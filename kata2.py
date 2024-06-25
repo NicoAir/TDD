aa4c345f0a5183e655aca1a63380b5b0b858a9eb
Nico <n-augier@hotmail.fr>
2024-06-25 13:44:40 +0200

    refacto

--- kata2.py
+++ kata2.py
@@ -3,16 +3,13 @@ import re
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    else:
-        delimiter = ',|\n'
-        if numbers.startswith('//'):
-            delimiter, numbers = numbers.split('\n', 1)
-            delimiter = delimiter[2:]
-            numbers_list = re.split(re.escape(delimiter), numbers)
-        else:
-            numbers_list = re.split(delimiter, numbers)
-        for num in numbers_list:
-            if not num.isdigit():
-                    position = numbers.find(num)
-                    raise ValueError(f"'{delimiter}' expected but '{num}' found at position {position}.")
-        return sum(int(num) for num in numbers_list if num.isdigit())
+    delimiter = ',|\n'
+    if numbers.startswith('//'):
+        delimiter, numbers = numbers.split('\n', 1)
+        delimiter = re.escape(delimiter[2:])
+    numbers_list = re.split(delimiter, numbers)
+    for num in numbers_list:
+        if not num.isdigit():
+            position = numbers.find(num)
+            raise ValueError(f"'{delimiter}' expected but '{num}' found at position {position}.")
+    return sum(int(num) for num in numbers_list if num.isdigit())
